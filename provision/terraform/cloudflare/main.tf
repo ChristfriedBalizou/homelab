a56af401244c111768056fa2d6cadaf445ad6517
Christfried <christfriedbalizou@gmail.com>
2023-05-25 13:41:52 +0000

    add: phpldapadmin to cloudflare terraform

--- provision/terraform/cloudflare/main.tf
+++ provision/terraform/cloudflare/main.tf
@@ -162,3 +162,12 @@ resource "cloudflare_record" "loft" {
   type    = "CNAME"
   ttl     = 1
 }
+
+resource "cloudflare_record" "phpldapadmin" {
+  name    = "phpldapadmin"
+  zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
+  value   = "ipv4.${data.sops_file.cloudflare_secrets.data["cloudflare_domain"]}"
+  proxied = true
+  type    = "CNAME"
+  ttl     = 1
+}
