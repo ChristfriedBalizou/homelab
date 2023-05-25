0fe925519e7e9bdd827748b310a66572552e5b3b
Christfried <christfriedbalizou@gmail.com>
2023-05-25 13:41:51 +0000

    change: remove ldap on cloudflare

--- provision/terraform/cloudflare/main.tf
+++ provision/terraform/cloudflare/main.tf
@@ -162,12 +162,3 @@ resource "cloudflare_record" "loft" {
   type    = "CNAME"
   ttl     = 1
 }
-
-resource "cloudflare_record" "ldap" {
-  name    = "ldap"
-  zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
-  value   = "ipv4.${data.sops_file.cloudflare_secrets.data["cloudflare_domain"]}"
-  proxied = true
-  type    = "CNAME"
-  ttl     = 1
-}
