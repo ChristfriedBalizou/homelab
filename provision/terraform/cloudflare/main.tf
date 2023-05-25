6ffd7ad5832e9c3a9a80b99a1ce3dd364901e741
Christfried <christfriedbalizou@gmail.com>
2023-05-25 13:41:50 +0000

    add: dex to terraform

--- provision/terraform/cloudflare/main.tf
+++ provision/terraform/cloudflare/main.tf
@@ -144,3 +144,12 @@ resource "cloudflare_record" "prometheus" {
   type    = "CNAME"
   ttl     = 1
 }
+
+resource "cloudflare_record" "dex" {
+  name    = "dex"
+  zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
+  value   = "ipv4.${data.sops_file.cloudflare_secrets.data["cloudflare_domain"]}"
+  proxied = true
+  type    = "CNAME"
+  ttl     = 1
+}
