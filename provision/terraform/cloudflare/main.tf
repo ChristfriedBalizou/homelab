30e3fa6ac2310747fbe4746441c13327b2d856a1
Christfried <christfriedbalizou@gmail.com>
2023-05-25 13:41:52 +0000

    admin: register ldap

--- provision/terraform/cloudflare/main.tf
+++ provision/terraform/cloudflare/main.tf
@@ -171,3 +171,12 @@ resource "cloudflare_record" "phpldapadmin" {
   type    = "CNAME"
   ttl     = 1
 }
+
+resource "cloudflare_record" "ldap" {
+  name    = "ldap"
+  zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
+  value   = "ipv4.${data.sops_file.cloudflare_secrets.data["cloudflare_domain"]}"
+  proxied = false
+  type    = "CNAME"
+  ttl     = 1
+}
