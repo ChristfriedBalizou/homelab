057408c82d101986a56b456a6ab1ed13eb8a5190
Christfried <christfriedbalizou@gmail.com>
2024-11-11 21:05:38 +0000

    admin: add loggers

--- .github/scripts/kromgo-badges.py
+++ .github/scripts/kromgo-badges.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import requests
 
 secret_domain = os.environ.get("SECRET_DOMAIN")
@@ -10,14 +11,19 @@ apikey = os.environ.get("KROMGO_AUTHELIA_APIKEY")
 
 
 def build_kromgo_url(tag: str, base_url: str = secret_domain):
+    print("Building kromgo url", file=sys.stdout)
     return f"https://kromgo.{secret_domain}/{tag}?format=badge&apikey={apikey}"
 
 
 def download_svg(tag: str):
     response = requests.get(build_kromgo_url(tag))
+    print("Downloaded badge with status: {response.status_code}", file=sys.stdout)
+
     response.raise_for_status()
 
-    with open(f"./kromgo/{tag}.sgv", "wb") as file_descriptor:
+    with open(f"./kromgo/{tag}.svg", "wb") as file_descriptor:
+        print("Saving badge", file=sys.stdout)
+
         for chunk in response:
             file_descriptor.write(chunk)
 
@@ -34,5 +40,5 @@ if __name__ == "__main__":
         try:
             download_svg(tag)
         except:
-            # fail in silent
+            print(f"Downloading badge {tag} failed.", file=sys.stderr)
             pass
